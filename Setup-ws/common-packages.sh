31090dd1bd9247ccf13aa00b0d9673206b75014e
Merthan Karaman <79408454+merthankaraman@users.noreply.github.com>
2023-01-25 17:40:22 +0300

    Update common-packages.sh

--- Setup-ws/common-packages.sh
+++ Setup-ws/common-packages.sh
@@ -1,12 +1,12 @@
 sudo apt update
 sudo apt upgrade --fix-missing
-sudo apt-get install python-opencv --fix-missing
-sudo apt-get install python3-opencv --fix-missing
+sudo apt-get install python-opencv
+sudo apt-get install python3-opencv
 #wget https://az764295.vo.msecnd.net/stable/57fd6d0195bb9b9d1b49f6da5db789060795de47/code_1.67.0-1651667246_amd64.deb -O vscode.deb
 #sudo dpkg -i vscode.deb && sudo rm vscode.deb
 sudo apt install snapd
 sudo snap install leafpad
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential --fix-missing
-sudo apt install curl --fix-missing
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install curl
 #sudo apt install freecad
 #sudo snap install fusion360 --beta --devmode
