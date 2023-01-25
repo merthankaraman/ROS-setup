6cfb50e7f5c19ae6f2ce4527c17b8073d7b376f6
Merthan Karaman <79408454+merthankaraman@users.noreply.github.com>
2023-01-25 17:40:00 +0300

    Update ROS-noetic-packages.sh

--- Setup-ws/ROS-noetic-packages.sh
+++ Setup-ws/ROS-noetic-packages.sh
@@ -2,7 +2,9 @@ yes | bash common-packages.sh
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full --fix-missing
+sudo apt update
+sudo apt --fix-broken install
+sudo apt install ros-noetic-desktop-full
 sudo rosdep init
 sudo rosdep fix-permissions
 rosdep update
