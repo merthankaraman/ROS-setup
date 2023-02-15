a9a48918ff871689bca83a79e4895608ce432f94
Merthan Karaman <79408454+merthankaraman@users.noreply.github.com>
2023-02-15 16:01:57 +0300

    hata giderildi

--- Setup-ws/ROS-noetic-packages.sh
+++ Setup-ws/ROS-noetic-packages.sh
@@ -2,10 +2,10 @@ yes | bash common-packages.sh
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
+sudo apt install python3-rosdep 
 sudo apt update
 sudo apt --fix-broken install
 sudo apt install ros-noetic-desktop-full
-sudo apt install python3-rosdep 
 sudo rosdep init
 sudo rosdep fix-permissions
 rosdep update
